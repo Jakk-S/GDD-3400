151552124a4e8fa09f98225e408ea4c82fa401ab
Jakk-S <jsteinbr@uccs.edu>
2025-09-26 00:05:48 -0600

    Comments

--- GDD 3400 Lab 01/Assets/GDD 3400 - Herding Sheep/Scripts/Dog.cs
+++ GDD 3400 Lab 01/Assets/GDD 3400 - Herding Sheep/Scripts/Dog.cs
@@ -7,11 +7,13 @@ namespace GDD3400.Project01
 {
     public class Dog : MonoBehaviour
     {
+        // States to control the dog's behavior
         enum DogStates
         {
             Scan,
             Chase
         }
+        private DogStates _state = DogStates.Scan;
 
         private bool _isActive = true;
         public bool IsActive 
@@ -24,6 +26,7 @@ namespace GDD3400.Project01
         private float _maxSpeed = 5f;
         private float _sightRadius = 7.5f;
 
+        // Turning and movement functionality
         private float _turnRate = 5f;
         private Rigidbody _rb;
 
@@ -37,16 +40,19 @@ namespace GDD3400.Project01
         private const string safeZoneTag = "SafeZone";
         private const string untagged = "Untagged";
 
-        private DogStates _state = DogStates.Scan;
+        // Timers and variables to control the robot dog scanning the level
         private float _timer = 0, firstTimer = 9f, xTimer = 13f, yTimer = 27f;
         private bool _across = false, _toStart = true, _chasing = false;
         private int _crossCount = 0, _sheepNum = 0;
 
+        // Used to change the Dog's tag to threat or friend or otherwise
         [SerializeField]
         private GameObject _collTag;
 
+        // Movement variables
         Vector3 targetPos = new Vector3(-17.5f, 0, 20), _velocity;
 
+        // Storing safe zone and seen sheep
         private Vector3 _startPos;
         private Vector3[] _sheepPos = new Vector3[12];
         private List<int> _savedSheep = new List<int>();
@@ -58,6 +64,7 @@ namespace GDD3400.Project01
             _targetsLayer = LayerMask.GetMask("Targets");
             _obstaclesLayer = LayerMask.GetMask("Obstacles");
 
+            // Gets rigidbody for movement and changes dog's tag
             _rb = GetComponent<Rigidbody>();
             _collTag.tag = friendTag;
         }
@@ -66,10 +73,13 @@ namespace GDD3400.Project01
         {
             if (!_isActive) return;
             
+            // sets the safe zone's position here, doesn't work in Awake
             if (_timer == 0) _startPos = transform.position;
 
+            // Shortens timer if spawning on top or left of level
             if ((_startPos.z >= 19 || _startPos.x <= -19) && _timer < 4) _timer = 5f;
 
+            // Timer functionality for scanning
             _timer += Time.deltaTime;
 
             Perception();
@@ -84,7 +94,7 @@ namespace GDD3400.Project01
                 var c = _tmpTargets[i];
                 if (c == null || c.gameObject == gameObject) continue;
 
-                // Store the friends, threat, and safe zone targets
+                // Store the sheep
                 switch (c.tag)
                 {
                     case friendTag:
@@ -104,6 +114,7 @@ namespace GDD3400.Project01
                 switch (_state)
                 {
                     case DogStates.Scan:
+                        // Functionality to scan the map across and down
                         if (_timer >= firstTimer && _toStart)
                         {
                             Debug.Log("REACHED DESTINATION");
@@ -131,6 +142,7 @@ namespace GDD3400.Project01
                         }
                         break;
                     case DogStates.Chase:
+                        // Lures the sheep back to the safe zone
                         targetPos = _startPos;
                         //if (!_chasing && !_savedSheep.Contains(_sheepNum))
                         //{
@@ -165,12 +177,12 @@ namespace GDD3400.Project01
         {
             if (!_isActive) return;
 
+            // speed control for scanning and luring sheep
             float speed = _maxSpeed;
+            if (!_toStart) speed *= .575f;
 
             // Calculate the direction to the target position
             Vector3 direction = (targetPos - transform.position).normalized;
-
-            if (!_toStart) speed *= .575f;
 
             // Calculate the movement vector
             _velocity = direction * Mathf.Min(speed, Vector3.Distance(transform.position, targetPos));
@@ -184,6 +196,7 @@ namespace GDD3400.Project01
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _turnRate);
             }
 
+            // Moves in target direction 
             _rb.linearVelocity = _velocity;
         }
     }
